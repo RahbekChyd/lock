fc39d4548ded2a1a82f776c4bd691b2a5283abbc
Mathias Rahbek <rahbekchyd@gmail.com>
2022-12-13 10:05:38 +0100

    Added boolean option in the ui configuration

--- src/connection/database/login_pane.jsx
+++ src/connection/database/login_pane.jsx
@@ -63,7 +63,7 @@ export default class LoginPane extends React.Component {
     const captchaPane =
       l.captcha(lock) &&
       l.captcha(lock).get('required') &&
-      (isHRDDomain(lock, databaseUsernameValue(lock)) || !sso) ? (
+      ((isHRDDomain(lock, databaseUsernameValue(lock)) || !sso) && !l.ui.disableCaptchaOnLogin) ? (
         <CaptchaPane i18n={i18n} lock={lock} onReload={() => swapCaptcha(l.id(lock), false)} />
       ) : null;
 
